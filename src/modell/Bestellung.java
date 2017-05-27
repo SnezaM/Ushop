9d94b9018a4340f5d5196fcbed887ff2761f2854
Katrin Rohrmüller <a1309572@unet.univie.ac.at>
2017-05-27 19:18:08 +0200

    Delete unused class Admin, Updated Bestellung

--- src/modell/Bestellung.java
+++ src/modell/Bestellung.java
@@ -1,8 +1,6 @@
-/**
- * 
- */
 package modell;
 
+import modell.Lieferart;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
@@ -20,7 +18,170 @@ public class Bestellung {
 	private String vermerk;
 	private Lieferart lieferart;
 	private LocalDate datum;
-	//private int kundenID;
+	private int kundenID;
 	
 
+	/**
+	 * Konstruktor. (Instanz fuer noch nicht abgeschlossene Bestellungen)
+	 * @param gesamtpreis Gesamtpreis der Bestellung.
+	 * @param positionen Liste mit den Positionen der Bestellung.
+	 * @param kundenID KundenID des Kunden der die Bestellung tätigt.
+	 */
+	public Bestellung(double gesamtpreis, List<Position> positionen, int kundenID) {
+		this.bestellungID = UUID.randomUUID();
+		this.gesamtpreis = gesamtpreis;
+		this.anzahl = positionen.size();
+		this.positionen = positionen;
+		this.abgeschlossen = false;
+		this.vermerk = null;
+		this.lieferart = null;
+		this.datum = null;
+		this.kundenID = kundenID;
+	}
+
+
+	/**
+	 * Retourniert den Gesamtpreis der Bestellung.
+	 * @return gesamtpreis
+	 */
+	public double getGesamtpreis() {
+		return gesamtpreis;
+	}
+
+
+	/**
+	 * Retourniert die Anzahl der Positionen der Bestellung.
+	 * @return anzahl
+	 */
+	public int getAnzahl() {
+		return anzahl;
+	}
+
+
+	/**
+	 * Retourniert eine Liste mit den Positionen
+	 * @return positionen
+	 */
+	public List<Position> getPositionen() {
+		return positionen;
+	}
+
+
+	/**
+	 * Retourniert, ob eine Bestellung abgeschlossen ist (true) oder nicht (false).
+	 * @return abgeschlossen
+	 */
+	public boolean isAbgeschlossen() {
+		return abgeschlossen;
+	}
+
+
+	/**
+	 * Retourniert den Vermerk einer Bestellung.
+	 * @return vermerk
+	 */
+	public String getVermerk() {
+		return vermerk;
+	}
+
+
+	/**
+	 * Retourniert die Lieferart einer Bestellung.
+	 * @return the lieferart
+	 */
+	public Lieferart getLieferart() {
+		return lieferart;
+	}
+
+
+	/**
+	 * Retourniert das Datum der Bestellung.
+	 * @return datum
+	 */
+	public LocalDate getDatum() {
+		return datum;
+	}
+
+
+	/**
+	 * Retourniert die KundenID des Bestellers.
+	 * @return kundenID
+	 */
+	public int getKundenID() {
+		return kundenID;
+	}
+
+	/**
+	 * Setzt den Gesamtpreis der Bestellung.
+	 * @param gesamtpreis Gesamtpreis, der gesetzt werden soll.
+	 */
+	public void setGesamtpreis(double gesamtpreis) {
+		this.gesamtpreis = gesamtpreis;
+	}
+
+
+	/**
+	 * Setzt die Anzahl der Positionen einer Bestellung.
+	 * @param anzahl the anzahl to set
+	 */
+	public void setAnzahl(int anzahl) {
+		this.anzahl = anzahl;
+	}
+
+
+	/**
+	 * Setzt die Liste der Positionen für die Bestellung.
+	 * @param positionen Liste mit Positionen, die gesetzt werden soll.
+	 */
+	public void setPositionen(List<Position> positionen) {
+		this.positionen = positionen;
+	}
+
+
+	/**
+	 * Setzt den Status für abgeschlossen für die Bestellung.
+	 * @param abgeschlossen Status, der für abgeschlossen gesetzt werden soll.
+	 */
+	public void setAbgeschlossen(boolean abgeschlossen) {
+		this.abgeschlossen = abgeschlossen;
+	}
+
+
+	/**
+	 * Setzt den Vermerk der Bestellung.
+	 * @param vermerk Vermerk, der gesetzt werden soll.
+	 */
+	public void setVermerk(String vermerk) {
+		this.vermerk = vermerk;
+	}
+
+
+	/**
+	 * Setzt die Lieferart der Bestellung.
+	 * @param lieferart Lieferart, die gesetzt werden soll.
+	 */
+	public void setLieferart(Lieferart lieferart) {
+		this.lieferart = lieferart;
+	}
+
+
+	/**
+	 * Setzt das Datum der Bestellung.
+	 * @param datum Datum, das gesetzt werden soll.
+	 */
+	public void setDatum(LocalDate datum) {
+		this.datum = datum;
+	}
+
+
+	/**
+	 * Setzt die KundenID des Bestellers für die Bestellung.
+	 * @param kundenID KundenID, die gestzt werden soll.
+	 */
+	public void setKundenID(int kundenID) {
+		this.kundenID = kundenID;
+	}
+	
+	//TODO updateAnzahl() entferne setAnzahl(int anzahl)
+	//TODO toString()
 }
